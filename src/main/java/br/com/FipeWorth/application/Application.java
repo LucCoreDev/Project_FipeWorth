bc5558318e182162d0464c07066c471702ddbfc1
LucCoreDev <lucasrcorreia23@gmail.com>
2025-03-03 01:40:55 -0300

    First commit

---
+++ src/main/java/br/com/FipeWorth/application/Application.java
@@ -0,0 +1,24 @@
+package br.com.FipeWorth.application;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+
+
+		sc.close();
+	}
+}
